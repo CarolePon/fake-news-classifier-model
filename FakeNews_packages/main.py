8d6393013e35b00b435513c3ac847b742c60305c
Baptiste <baptiste.depreneuf@gmail.com>
2024-11-28 17:31:28 +0100

    preproc almost done

--- FakeNews_packages/main.py
+++ FakeNews_packages/main.py
@@ -1,17 +1,9 @@
 
 from data import get_data_text_title_df, get_data_text_df
-
 # Get the dataframe to run model with text only
 data_title_text = get_data_text_title_df()
-
-
-
 # Get the dataframe to run model with title and text
 data_text_only = get_data_text_df()
-
-
-
-
 #if __name__ == "__main__":
     # print(data_title_text.head())
     # print(data_text_only.head())
--- FakeNews_packages/params.py
+++ FakeNews_packages/params.py
@@ -1,14 +1,21 @@
-from from data import get_data_text_title_df
+from data import get_data_text_title_df
 
 """
 PARAMS PREPROCESSING
 """
 
-# words limits:
 df = get_data_text_title_df
-column_name = '' # "title" or "text"
-high_lim = 5000  # high lim of words
-low_lim = # low lim of words
+
+# words limits title:
+column_title = 'title' # "title" or "text"
+high_lim_title = 250  # high lim of words
+low_lim_title =  0 # low lim of words
+
+# words limits title:
+column_article = 'text' # "title" or "text"
+high_lim_article = 5000  # high lim of words
+low_lim_article =  0 # low lim of words
+
 
 # column choice: for the function to return either a df with only the title + label; only the text + label or text + title concatenated + label
 title = False
@@ -42,10 +49,10 @@ rem_pun = True
 keep_h = False
 
 # PREPROC_TEXT
-
+clean_text = True
 tokens = True # do not change
 
-Stopwords = True
+stpwords = True
 
 language = 'english' #for lemmatizing
 
@@ -58,15 +65,3 @@ adjectives = True
 sat_adj = True
 
 reassemble_txt = True
-
-
-
-def preproc_txt(txt: str, clean_txt: bool=True, strip: bool=True, remove_links_bool: bool=True,
-                remove_selected_words_bool: bool=False, list_words_to_remove: List[str]=[''],lower: bool=True,
-                remove_digits: bool=True, remove_punctuation: bool=True,
-                keep_hashtags: bool=False, tokenize: bool=True,
-                stopwords: bool=True, language: str='english',
-                lemmatize: bool=True, nouns: bool=False, verbs: bool=False,
-                adjectives: bool=False, adverbs: bool=False,
-                satellite_adjectives: bool=False,
-                reassemble_txt: bool=True) -> str:
